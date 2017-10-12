9ca26579f0830cb1b91bad84d6a50760a14380c2
Kirill Popov <kpopov@griddynamics.com>
2017-10-12 06:04:11 +0300

    Added firewall rule to allow 8080 port

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 WORK_DIR="/tmp/www"
+LISTEN_PORT=8080
+
+/bin/firewall-cmd --add-port=${LISTEN_PORT}/tcp --permanent
+/bin/firewall-cmd --reload
 
 mkdir "$WORK_DIR"
 echo "File served by $(hostname)" > "$WORK_DIR/index.txt"
 
 cd "$WORK_DIR"
-python -m SimpleHTTPServer 8080 &
+python -m SimpleHTTPServer $LISTEN_PORT &
 disown
 exit 0
